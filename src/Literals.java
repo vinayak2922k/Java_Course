6b40dae3e4dd48e2f63cee2ca11c14bf50306324
vinayak2922k <vinayaksharma2289@gmail.com>
2026-05-18 01:25:38 +0530

    init commit

---
+++ src/Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args)
+    {
+        System.out.println("Here is the literals");
+        int a = 10;
+        byte age = 18;
+        long salary = 1000000000L;
+        char Name = 'c';
+        float F1 = 5.66f;
+        double D1 = 4.7D;
+
+        System.out.println("Here is all literals listed above :");
+
+    }
+}
---
+++ src/Taking_Input.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Taking_Input {
+    public static void main(String[] args) {
+        System.out.println("Taking inputs from the user");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Num1");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the Num2");
+        int num2 = sc.nextInt();
+        int Sum = num1 + num2;
+        System.out.println("Here is the Sum :" + Sum);
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
