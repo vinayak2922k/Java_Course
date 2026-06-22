4f6f4ca2c46646412c202f3b2afb042d4af2e43f
vinayak2922k <vinayaksharma2289@gmail.com>
2026-06-22 12:50:12 +0530

    init commit

--- src/java_time.java
+++ src/java_time.java
@@ -3,13 +3,15 @@ import java.time.LocalTime;
 import java.time.LocalDateTime;
 
 public class java_time {
-    LocalDate d = LocalDate.now();
-    System.out.println(d);
+    public static void main(String args[]) {
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
 
-    LocalTime t = LocalTime.now();
-    System.out.println(t);
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
 
-    LocalDateTime dt = LocalDateTime.now();
-    System.out.println(dt);
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
 
 }
